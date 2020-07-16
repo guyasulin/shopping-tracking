7ab2dea8be629b1e44ecd3e4e806a6ff3c77aba1
guy <guyasulin13@gmail.com>
2020-07-17 00:06:53 +0300

    .

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/shopping-tracking-list/dist'));
+app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
-app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname + '/shopping-tracking-list/dist/shopping-tracking-list/index.html'))
+app.get('/*', function(req, res){
+    res.sendFile(path.join(__dirname + '/dist/shopping-tracking-list/index.html'))
 })
 
 console.log('Console Listening!');
