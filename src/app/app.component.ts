eb5fced55f14ccc21b0a8d3defe48a94b6688bc1
guy <guyasulin13@gmail.com>
2020-07-17 08:41:36 +0300

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { environment } from './../environments/environment';
 import { Component, OnInit } from '@angular/core';
 import { ItemService } from './items/services/item.service';
 
@@ -6,7 +7,7 @@ import { ItemService } from './items/services/item.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Shopping Tracking List';
 
   constructor( public itemService:ItemService) {
@@ -14,5 +15,17 @@ export class AppComponent {
     
   }
 
+  ngOnInit() {
+    this.loadScript();
+}
 
+  private loadScript() {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.innerHTML = 'window["adrum-start-time"] = new Date().getTime(); (function(config){ config.appKey = ' + environment.production + '; })(window["adrum-config"] || (window["adrum-config"] = {}));';
+    const head = document.getElementsByTagName('head')[0];
+    if (head !== null && head !== undefined) {
+      document.head.appendChild(script);
+    }
+}
 }
